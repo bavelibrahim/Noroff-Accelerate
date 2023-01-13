56de177a33a1c33d0ee0d8c132e993e0d73e39bb
Pavel Shwan Ibrahim <pavel.shwan.ibrahim@no.experis.com>
2023-01-13 13:22:11 +0100

    Added changes to work and salary segments of Assignment01

--- Assignment01/app.js
+++ Assignment01/app.js
@@ -1,11 +1,19 @@
+// ? GLOBAL VARIABLES
+
+let gotLoan = false
+let outstandingValueBoolean = false
+
+// ???????????????
 
 // A template for creating a bank user. 
-function bankUser(Name, Surname, Balance, Salary){
+function bankUser(Name, Surname, Balance, Salary, SalaryBalance, outLoan){
     // Properties of the Manager in his/her branch
     this.Name               = Name
     this.Surname            = Surname
     this.Balance            = Balance
     this.Salary             = Salary
+    this.SalaryBalance      = SalaryBalance
+    this.out_Loan           = outLoan
 }
 
 // Creating Gandalf the Gray as a bank user. 
@@ -13,57 +21,59 @@ let GrayGandalf = new bankUser(
     "Gandalf",
     "The Brown",
     300,
-    120,
+    100,
+    0,
+    0
 )
 
+// Loading elements when window is loaded....
 window.onload = function(){
+
+    //! FIXED TEXT ELEMENTSS
     document.getElementById('Gandalf').innerHTML = GrayGandalf.Name + " " + GrayGandalf.Surname
-    document.getElementById('btn-getLoan').addEventListener("click", getLoan);
+
+    //! BUTTONS
+    document.getElementById('btn-getLoan').addEventListener("click", getLoan)
+    document.getElementById('btn-doWork').addEventListener("click", work)
+
+    //! CHANGEABLE TEXT ELEMENTS
+    document.getElementById('text-per-balance').innerHTML = "Balance:   " + GrayGandalf.SalaryBalance
     document.getElementById('text-Balance').innerHTML = "Balance:   " + GrayGandalf.Balance
 }
 
-// Assign the Loan boolean as false because the loan has not been received yet
-let gotLoan = false
-
-
 // getLoan function checks for the requirements provided by the assignment document
-function getLoan(amount){
+function getLoan(){
     
-    amount = 0
     //? Prompt popping up when asking for an amount for the Loan
-    amount = Number(window.prompt("Type the amount for the Loan "))
-    console.log("Gandalf got: " + GrayGandalf.Balance)
-    
-    if (amount >= ((GrayGandalf.Balance)*2))
-        amount = 0 
-        alert("The amount cannot be double your balance, please try again.")
-        getLoan()
+    amount = Number(window.prompt("Type the amount for the Loan"))
 
-    if(gotLoan == true)
-        alert("You have already received a loan, there will be no second loan before the previous one is repaid.")
+    while (amount >= (GrayGandalf.Balance * 2) && gotLoan == false){
+        amount = Number(window.prompt("The amount cannot be double or more, please type the amount for the Loan: "))
+    }
     
-    else {
-        console.log("Getting loan....")
-        console.log("Loan amount is: " + amount)
+    if(gotLoan == true && amount != 0){
+        alert("You have already received a loan, there will be no second loan before the previous one is repaid.")
+        amount = 0
+    }
+
+    else if (gotLoan == false){
+        document.getElementById('text-outstanding-loan')
+        .innerHTML = "Loan: " + amount
         gotLoan = true
-        GrayGandalf.Balance = GrayGandalf.Balance + amount
-        document.getElementById('text-Balance').innerHTML = "Balance:   " + GrayGandalf.Balance
+        newBalance = GrayGandalf.Balance + amount
+        document.getElementById('text-Balance')
+        .innerHTML = "Balance: " + newBalance
     }
 }
 
-let outstandingValueBoolean = false
+// the function Work adds a certain amount of money to the users personal balane before adding it to the bank
+function work(){
+    GrayGandalf.SalaryBalance += 100
+    document.getElementById('text-per-balance')
+    .innerHTML = "Balance:   " + GrayGandalf.SalaryBalance
+}
 
 function checkLoanRepaid(Amount){
     if (Amount == 0)
         gotLoan = false       
 }
-
-function add(value, add){
-    return value + add
-}
-
-function subtract(value, subtract) {
-    return value - subtract
-}
-
-
