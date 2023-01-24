f8a740041de9cabf5363498c9700aa87b41a804a
Pavel Shwan Ibrahim <pavel.shwan.ibrahim@no.experis.com>
2023-01-24 09:18:35 +0100

    Added redirect to profile page after logging in

--- Assignment02/translateeor/src/components/Login/LoginForm.jsx
+++ Assignment02/translateeor/src/components/Login/LoginForm.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { loginUser } from "../../api/user";
-
+import { storageSave } from "../../utils/storage";
+import { useUser } from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
 const unConfig = {
   required: true,
@@ -9,18 +11,35 @@ const unConfig = {
 };
 
 const LoginForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
 
+  const { register, handleSubmit, formState: { errors }, } = useForm();
+  const { user, setUser } = useUser()
+  const navigate = useNavigate()
+
+  //! These are the local states
   const [ loading, setLoading ] = useState(false)
+  const [ apiError, setApiError ] = useState(false)
 
+  //! These are side effects
+  useEffect(() => {
+    if (user !== null){
+      navigate('profile')
+    }
+  }, [ user, navigate])
+
+  //! These are Event Handlers
   const onSubmit = async ({ username }) => {
-    const [error, user] = await loginUser(username)
-    console.log('Error: ', error)
-    console.log('User: ', user);
+    setLoading(true)
+    const [error, userResponse] = await loginUser(username)
+    if (error !== null){
+      setApiError(error)
+    }
+    if (userResponse !== null) {
+      storageSave('user-transl', userResponse)
+      setUser(userResponse)
+    }
+    setLoading(false)
+
   };
 
   return (
@@ -34,6 +53,7 @@ const LoginForm = () => {
         <button type="submit" disabled={ loading } >Continue</button>
 
         { loading && <p>Logging in...</p>}
+        { apiError && <p>{ apiError }</p>}
       </form>
     </>
   );
